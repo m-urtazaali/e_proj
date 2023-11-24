d19fdb31656e61789ef16d16cf178a3c7d526877
Murtaza <murtazaali3014@gmail.com>
2023-11-24 15:22:08 +0500

    main

--- lib/register.dart
+++ lib/register.dart
@@ -16,186 +16,186 @@ class _MyRegisterState extends State<MyRegister> {
     return Container(
       child: Scaffold(
         backgroundColor: Colors.white,
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-        ),
-        body: SingleChildScrollView(
-          child: Stack(
-            children: [
-              Container(
-                padding: EdgeInsets.only(left: 35, top: 30),
-                child: Text(
-                  'Create an \naccount',
-                  style: TextStyle(color: Colors.black, fontSize: 40, fontWeight: FontWeight.w800),
+
+        body: Padding(
+          padding: const EdgeInsets.only(top: 55.0),
+          child: SingleChildScrollView(
+            child: Stack(
+              children: [
+                Container(
+                  padding: EdgeInsets.only(left: 35, top: 30),
+                  child: Text(
+                    'Create an \naccount',
+                    style: TextStyle(color: Colors.black, fontSize: 40, fontWeight: FontWeight.w800),
+                  ),
                 ),
-              ),
-              Container(
-                margin: EdgeInsets.only(left: 35, right: 35),
-                padding: EdgeInsets.only(
-                    top: MediaQuery.of(context).size.height * 0.2),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                   Column(
+                Container(
+                  margin: EdgeInsets.only(left: 35, right: 35),
+                  padding: EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height * 0.2),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                     Column(
+                          children: [
+                            TextFormField(
+                              controller: email,
+                              style: TextStyle(color: Colors.black),
+                              decoration: InputDecoration(
+                                  fillColor: Colors.grey.shade100,
+                                  filled: true,
+                                  prefixIcon: Icon(Icons.person),
+                                  // prefix: IconButton(icon : Icon(Icons.person), onPressed: (){},color: Colors.black),
+                                  hintText: "Username Or Email",
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  )),
+                            ),
+                            const SizedBox(
+                              height: 30,
+                            ),
+                            TextFormField(
+                              controller: password,
+                              style: TextStyle(),
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                  fillColor: Colors.grey.shade100,
+                                  filled: true,
+                                  hintText: "Password",
+                                  prefixIcon: Icon(Icons.lock),
+                                  // enabledBorder: OutlineInputBorder(
+                                  //
+                                  // ),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  )),
+                            ),
+                            SizedBox(
+                              height: 30,
+                            ),
+                            TextFormField(
+                              controller: password,
+                              style: TextStyle(),
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                  fillColor: Colors.grey.shade100,
+                                  filled: true,
+                                  hintText: "Confirm Password",
+                                  prefixIcon: Icon(Icons.lock),
+                                  // enabledBorder: OutlineInputBorder(
+                                  //
+                                  // ),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  )),
+                            ),
+                          ],
+                        ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text("By clicking the Register button, you agree to the public offer",style: TextStyle(
+                          color: Colors.black38,
+                          fontSize: 16
+                      ),),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Container(
+                          width: MediaQuery.of(context).size.width,
+                          height: MediaQuery.of(context).size.height / 14,
+                          child: OutlinedButton(
+                            onPressed: () {},
+                            child: Text(
+                              'Create Account',
+                              style: TextStyle(
+                                  color: Colors.white, fontSize: 20),
+                            ),
+                            style: OutlinedButton.styleFrom(
+                              backgroundColor: Colors.redAccent,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                            ),
+                          )),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      Center(
+                        child: Column(
+                          children: [
+                            Text(
+                              "- OR Continue With -",
+                              style: TextStyle(fontSize: 15),
+                            )
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          TextFormField(
-                            controller: email,
-                            style: TextStyle(color: Colors.black),
-                            decoration: InputDecoration(
-                                fillColor: Colors.grey.shade100,
-                                filled: true,
-                                prefixIcon: Icon(Icons.person),
-                                // prefix: IconButton(icon : Icon(Icons.person), onPressed: (){},color: Colors.black),
-                                hintText: "Username Or Email",
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                )),
-                          ),
-                          const SizedBox(
-                            height: 30,
-                          ),
-                          TextFormField(
-                            controller: password,
-                            style: TextStyle(),
-                            obscureText: true,
-                            decoration: InputDecoration(
-                                fillColor: Colors.grey.shade100,
-                                filled: true,
-                                hintText: "Password",
-                                prefixIcon: Icon(Icons.lock),
-                                // enabledBorder: OutlineInputBorder(
-                                //
-                                // ),
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                )),
+                          CircleAvatar(
+                            radius: 15,
+                            backgroundImage:
+                            AssetImage('assets/google.png'),
+                            backgroundColor: Colors.transparent,
                           ),
                           SizedBox(
-                            height: 30,
+                            width: 18,
                           ),
-                          TextFormField(
-                            controller: password,
-                            style: TextStyle(),
-                            obscureText: true,
-                            decoration: InputDecoration(
-                                fillColor: Colors.grey.shade100,
-                                filled: true,
-                                hintText: "Confirm Password",
-                                prefixIcon: Icon(Icons.lock),
-                                // enabledBorder: OutlineInputBorder(
-                                //
-                                // ),
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                )),
+                          CircleAvatar(
+                            radius: 18,
+                            backgroundImage:
+                            AssetImage('assets/apple logo.png'),
+                            backgroundColor: Colors.transparent,
+                          ),
+                          SizedBox(
+                            width: 18,
+                          ),
+                          CircleAvatar(
+                            radius: 18,
+                            backgroundImage:
+                            AssetImage('assets/facebook.png'),
+                            backgroundColor: Colors.transparent,
                           ),
                         ],
                       ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Text("By clicking the Register button, you agree to the public offer",style: TextStyle(
-                        color: Colors.black38,
-                        fontSize: 16
-                    ),),
-                    SizedBox(
-                      height: 30,
-                    ),
-                    Container(
-                        width: MediaQuery.of(context).size.width,
-                        height: MediaQuery.of(context).size.height / 14,
-                        child: OutlinedButton(
-                          onPressed: () {},
-                          child: Text(
-                            'Create Account',
-                            style: TextStyle(
-                                color: Colors.white, fontSize: 20),
-                          ),
-                          style: OutlinedButton.styleFrom(
-                            backgroundColor: Colors.redAccent,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(12),
-                            ),
-                          ),
-                        )),
-                    SizedBox(
-                      height: 40,
-                    ),
-                    Center(
-                      child: Column(
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           Text(
-                            "- OR Continue With -",
-                            style: TextStyle(fontSize: 15),
+                            "I Already Have an Account",
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).push(
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          MyLogin()));
+                            },
+                            child: Text("Login",
+                                style: TextStyle(
+                                    color: Colors.redAccent,
+                                    fontSize: 20,
+                                    decoration:
+                                    TextDecoration.underline)),
                           )
                         ],
-                      ),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        CircleAvatar(
-                          radius: 15,
-                          backgroundImage:
-                          AssetImage('assets/google.png'),
-                          backgroundColor: Colors.transparent,
-                        ),
-                        SizedBox(
-                          width: 18,
-                        ),
-                        CircleAvatar(
-                          radius: 18,
-                          backgroundImage:
-                          AssetImage('assets/apple logo.png'),
-                          backgroundColor: Colors.transparent,
-                        ),
-                        SizedBox(
-                          width: 18,
-                        ),
-                        CircleAvatar(
-                          radius: 18,
-                          backgroundImage:
-                          AssetImage('assets/facebook.png'),
-                          backgroundColor: Colors.transparent,
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "I Already Have an Account",
-                          style: TextStyle(fontSize: 20),
-                        ),
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context).push(
-                                MaterialPageRoute(
-                                    builder: (context) =>
-                                        MyLogin()));
-                          },
-                          child: Text("Login",
-                              style: TextStyle(
-                                  color: Colors.redAccent,
-                                  fontSize: 20,
-                                  decoration:
-                                  TextDecoration.underline)),
-                        )
-                      ],
-                    )
-                  ],
+                      )
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
