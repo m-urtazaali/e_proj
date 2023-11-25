77f0bd3fc6fd2ff783532387691dc2a776d4edb8
Murtaza <murtazaali3014@gmail.com>
2023-11-25 16:07:12 +0500

    main

--- lib/BannerPage.dart
+++ lib/BannerPage.dart
@@ -10,15 +10,67 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/bannermob.jpg"),
-            fit: BoxFit.cover,
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        image: DecorationImage(image: AssetImage("assets/bannermob.jpg"), fit: BoxFit.cover),
+      ),
+      child: Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.fromLTRB(3,440,3,30),
+          child: Column(
+            children: [
+              Text("You want",style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40,
+                  fontWeight: FontWeight.w900,
+                  decoration: TextDecoration.none
+              ),),
+              Text("Authentic? Here",style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w900,
+                  fontSize: 40,
+                  decoration: TextDecoration.none
+              ),),
+              Text("you go!",style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40,
+                  fontWeight: FontWeight.w900,
+                  decoration: TextDecoration.none
+              ),)
+            ],
           ),
         ),
-        child: null /* add child content here */,
+        Text("Find it here, buy it now!",style: TextStyle(
+        color: Colors.white,
+        fontSize: 20,
+        fontWeight: FontWeight.normal,
+          decoration: TextDecoration.none
+        )
+        ),
+        SizedBox(height: 50,),
+        Container(
+            width: MediaQuery.of(context).size.width/1.3,
+            height: MediaQuery.of(context).size.height / 14,
+            child: OutlinedButton(
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => SplashScreen()));
+              },
+              child: Text(
+                'Get Started',
+                style: TextStyle(
+                    color: Colors.white, fontSize: 30),
+              ),
+              style: OutlinedButton.styleFrom(
+                backgroundColor: Colors.redAccent,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+            )),
+
+      ],
       ),
     );
   }
